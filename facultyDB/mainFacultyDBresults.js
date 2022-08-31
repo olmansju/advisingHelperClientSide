8447125ad38d524d0ab7eba57d9ef884c354c363
Justin Olmanson <olmansju@gmail.com>
2022-08-31 11:14:37 -0500

    display results tweak

--- facultyDB/mainFacultyDBresults.js
+++ facultyDB/mainFacultyDBresults.js
@@ -1,6 +1,7 @@
 // references mongoDBtraffic.js function
 
 function displayResults(theReturnedJSON){
+    console.log('displayResults', theReturnedJSON.body.payload);
     document.getElementById("facultyDBresults").innerHTML = `<PRE><CODE> ${theReturnedJSON.payload} </CODE></PRE>`;
 }
 
